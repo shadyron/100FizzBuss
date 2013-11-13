365c4d07f1a83e8e8f823fb11508a0113e076bf7
shadyron <github@l33t-d00d.co.uk>
2013-11-13 14:37:25 +0000

    Revert "Added in without debug (just shows words without numbers in brackets)"
    This reverts commit 6892207dea373c4e9d8bdb5ee1ade5ebeade28fd.

--- 100fizzbuzz.php
+++ 100fizzbuzz.php
@@ -2,11 +2,11 @@
 
 for ($i=1; $i<101; $i++) {
         if ((is_int(($i)/5)) AND (is_int(($i)/3))) {
-                echo "FizzBuzz (" . $i . ")<br>";
+                echo "FizzBuzz<br>";
         } else if (is_int(($i)/5)) {
-                echo "Buzz (" . $i . ")<br>";
+                echo "Buzz<br>";
         } else if (is_int(($i)/3)) {
-                echo "Fizz (" . $i . ")<br>";
+                echo "Fizz<br>";
         }
 
         echo $i . "<br>";
