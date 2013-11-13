d25886f1567fb23d908ed6472ad57117a5d8fc49
shadyron <github@l33t-d00d.co.uk>
2013-11-13 14:39:35 +0000

    Version that shows numbers in brackets

---
+++ 100fizzbuzz-with-numbers.php
@@ -0,0 +1,14 @@
+<?
+
+for ($i=1; $i<101; $i++) {
+        if ((is_int(($i)/5)) AND (is_int(($i)/3))) {
+                echo "FizzBuzz (" . $i . ")<br>";
+        } else if (is_int(($i)/5)) {
+                echo "Buzz (" . $i  . ")<br>";
+        } else if (is_int(($i)/3)) {
+                echo "Fizz (" . $i . ")<br>";
+        }
+
+        echo $i . "<br>";
+}
+?> 
